a157b2b6f81a197d159e12e0c354703dd110b983
AshanSandeep06 <ashansandeep06@gmail.com>
2024-01-21 14:17:26 +0530

    SellHouse Component has been added

--- frontend/src/pages/SellerPages/SellHouse/SellHouse.tsx
+++ frontend/src/pages/SellerPages/SellHouse/SellHouse.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import Header from "../../../components/Header";
+import "./SellHouse.css";
 
 const SellHouse = () => {
   return (
-    <div>SellHouse</div>
-  )
-}
+    <>
+      <Header
+        buttons={["logout", "my_profile"]}
+        links={["my_listings", "predict_house_price", "sell_your_house"]}
+        prefix="/user/"
+      />
+
+      <main className="mt-20 gap-2 w-full pt-6 px-10 bg-[#FAFAFF] h-[calc(100vh-80px)]">
+        <main className="gap-2 w-full px-10 bg-[#FAFAFF]">
+          <h1
+            className="text-center relative !pb-[8px] mb-[35px]"
+            id="foodMenuHeading2"
+          >
+            Sell Your House
+          </h1>
+        </main>
+      </main>
+    </>
+  );
+};
 
 export default SellHouse;
